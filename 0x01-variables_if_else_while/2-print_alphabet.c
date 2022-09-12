8c6d22f5b3184d77c6e217a996f3be092ad6b93f
Pamchema <urechique@gmail.com>
2022-09-12 05:17:44 -0700

    yy

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	for (i =97; i <= 122; i++){
+
+	for (i = 97; i <= 122; i++)
+	{
 		putchar(i);
+		if (i == 122)
+			printf(" ");
+
 	}
 	return (0);
 }
