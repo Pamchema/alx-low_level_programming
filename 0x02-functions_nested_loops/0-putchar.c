c1b8d1615f18cf5eeb17dbdbefd88eac3f405644
Pamchema <urechique@gmail.com>
2022-09-13 19:17:53 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Printing _putchar message
+ *
+ * Return:  Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char c[8] = "_putchar";
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(c[i]);
+
+	}
+	_putchar('\n');
+		return (0);
+}
