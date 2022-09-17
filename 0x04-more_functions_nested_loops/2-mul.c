54d211faf52f1032d6060892fd4e8918297b3adf
Pamchema <urechique@gmail.com>
2022-09-16 18:39:26 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * mul -multiple two integers
+ *
+ * Return: Multiplication of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@
  */
 void print_numbers(void)
 {
-	int i;
+	char i;
 
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
